49755b06bc31c725a5bc763ae678baf12fd9fe2f
stephany <sny.17@hotmail.com>
2024-03-19 13:50:02 +0000

    proyecto alededor del mundo

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Tripleten web_project_around
+
+Este proyecto fue desarrollado con las siguientes tecnologías:
+
+HTML
+CSS
+javascript
+Popup 
+Git y GitHub
+Metodología BEM
+
+Este es el Link para ingreso:
+
---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+const openEditButton = document.querySelector("#editbutton");
+const openProfileButton = document.querySelector("#addbutton");
+const popupProfile = document.querySelector(".popup_profile");
+const popupPlace = document.querySelector(".popup_place");
+const popupCloseIcon = document.querySelectorAll(".popup__close-icon");
+const formElement = document.querySelector(".popup__container");
+
+const profileText = document.querySelector(".profile__text");
+const profileTitle = document.querySelector(".profile__title");
+
+const nameInput = formElement.querySelector(".popup__input-name");
+const aboutMeInput = formElement.querySelector(".popup__input-about");
+
+openEditButton.addEventListener("click", function () {
+  popupProfile.classList.add("popup_open");
+});
+
+openProfileButton.addEventListener("click", function () {
+  popupPlace.classList.add("popup_open");
+  aboutMeInput.value = profileText.textContent;
+  nameInput.value = profileTitle.textContent;
+});
+
+popupCloseIcon.forEach((item) => {
+  item.addEventListener("click", function () {
+    popupProfile.classList.remove("popup_open");
+    popupPlace.classList.remove("popup_open");
+  });
+});
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileText.textContent = aboutMeInput.value;
+  profileTitle.textContent = nameInput.value;
+  popupProfile.classList.remove("popup_open");
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
